2ba14010c29baddfd8d90f917b3adb141435a263
Anaghasarige <103187642+Anaghasarige@users.noreply.github.com>
2022-05-23 13:42:49 +0530

    Update EmployeeController.java

--- Employee/src/main/java/com/example/Employee/Controller/EmployeeController.java
+++ Employee/src/main/java/com/example/Employee/Controller/EmployeeController.java
@@ -31,7 +31,7 @@ public List<Employee> displayMessage(){
 	return employeeRepository.findAll(); 
 	
 }
-//Save all Employees
+//Save the all Employees
 @PostMapping("/employees")
 public Employee addEmployee(@RequestBody Employee emp) {
 	Employee getEmp = employeeRepository.save(emp);
